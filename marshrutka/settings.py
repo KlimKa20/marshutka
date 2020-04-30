e5092af44d3d2c47f81ba5a59c04c17e0de80831
AlinaSadovskaya <47573337+AlinaSadovskaya@users.noreply.github.com>
2020-04-30 11:31:58 +0300

    Update settings.py

--- marshrutka/settings.py
+++ marshrutka/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'book/static/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
