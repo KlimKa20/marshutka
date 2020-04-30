d48f3c3e1626812476b9e702b4abaa781fea2e6f
AlinaSadovskaya <47573337+AlinaSadovskaya@users.noreply.github.com>
2020-04-30 13:59:21 +0300

    Update settings.py

--- marshrutka/settings.py
+++ marshrutka/settings.py
@@ -161,7 +161,4 @@ LOGGING = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
