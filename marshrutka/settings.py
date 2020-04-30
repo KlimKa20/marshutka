36bd10c2a928b181e1c3dfefdad0a3aca510e364
AlinaSadovskaya <47573337+AlinaSadovskaya@users.noreply.github.com>
2020-04-30 10:03:26 +0300

    Update settings.py

--- marshrutka/settings.py
+++ marshrutka/settings.py
@@ -118,8 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'alinasadovsckaya@yandex.ru'
