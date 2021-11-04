e15662144abf2c64f9d4ce4c41f754ff7ea7c242
KlimKa20 <62294637+KlimKa20@users.noreply.github.com>
2021-11-04 20:33:20 +0300

    Add comment button

--- firstpaige/models.py
+++ firstpaige/models.py
@@ -12,7 +12,7 @@ class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('-created',)
 
     def __str__(self):
         return self.comment
